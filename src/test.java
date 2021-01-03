f387097decd947292d984acf21254fd0763bc2b9
Anna Karutina <anna.karutina@khk.ee>
2021-01-04 00:55:37 +0200

    if-else - ülesanne 2.1

--- src/test.java
+++ src/test.java
@@ -1,17 +1,25 @@
 /*
  * Muutujate kasutamise näide
  * autor - Anna Karutina
- * ülesanne 1.2
+ * ülesanne 2.1
  * */
+
+import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
         // defineerime muutujad
-        int aasta = 2020;
-        String liblikas = "teelehe-mosaiikliblikas";
-        String lauseKeskosa = ". aasta liblikas on ";
-        String lause = aasta + lauseKeskosa + liblikas;
-        // väljastamine
-        System.out.println(lause);
+        // lubame kasutajale sisestada midagi
+        Scanner sisend = new Scanner(System.in);
+        // selgitame kasutajale, mida tuleb sisestada
+        System.out.println("Palun sisesta pilvede kõrgus: ");
+        // salvestame kasutaja sisestatud väärtus
+        double pilvedeKorgus = sisend.nextDouble();
+        // kontroll
+        if (pilvedeKorgus > 6.0) {
+            System.out.println("Need on ülemised pilved");
+        } else {
+            System.out.println("Need ei ole ülemised pilved");
+        }
     }
 }
