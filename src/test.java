440541a36d1416423c6b3e0f6ccd0ec90589f1c0
Anna Karutina <anna.karutina@khk.ee>
2021-01-07 22:08:02 +0200

    meetodid - ülesanne 6.2

--- src/test.java
+++ src/test.java
@@ -1,16 +1,16 @@
 /*
  * meetod + return -  näide
  * autor - Anna Karutina
- * ülesanne 6.1
+ * ülesanne 6.2
  * */
 
 import java.util.Scanner;
 
 public class test {
-    // banner meetod
-    static String banner(String reklaamlause) {
-        String reklaamlauseSuurteTahtedega = reklaamlause.toUpperCase();
-        return reklaamlauseSuurteTahtedega;
+    // mahlapakkideArv meetod
+    static int mahlapakkideArv(double ounteKogus) {
+        int pakkideArv = (int) Math.round(ounteKogus * 0.4 / 3);
+        return pakkideArv;
     }
 
     // main meetod
@@ -19,18 +19,10 @@ public class test {
         // lubame kasutajale sisestada midagi
         Scanner sisend = new Scanner(System.in);
         // selgitame kasutajale, mida tuleb sisestada
-        System.out.println("Mitu korda kuvada reklaamlauset: ");
+        System.out.println("Sisestage õunte kogus kilogrammides: ");
         // salvestame kasutaja sisestatud väärtus
-        int kordamine = sisend.nextInt();
-        // samuti sisendid
-        sisend = new Scanner(System.in);
-        System.out.println("Sisesta reklaamlause: ");
-        String lause = sisend.nextLine();
-        // kordamine
-        int kord = 1;
-        while (kord <= kordamine) {
-            System.out.println(banner(lause));
-            kord++;
-        }
+        double ountekogus = sisend.nextDouble();
+        // arvutamine
+        System.out.println(mahlapakkideArv(ountekogus));
     }
 }
