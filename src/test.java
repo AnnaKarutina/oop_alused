d811868d821ea41c09809a3ab28f8d0586062aa9
Anna Karutina <anna.karutina@khk.ee>
2021-01-07 22:01:32 +0200

    meetodid - ülesanne 6.1

--- src/test.java
+++ src/test.java
@@ -1,28 +1,36 @@
 /*
- * while tsükkel -  näide
+ * meetod + return -  näide
  * autor - Anna Karutina
- * ülesanne 3.3
+ * ülesanne 6.1
  * */
 
 import java.util.Scanner;
 
 public class test {
+    // banner meetod
+    static String banner(String reklaamlause) {
+        String reklaamlauseSuurteTahtedega = reklaamlause.toUpperCase();
+        return reklaamlauseSuurteTahtedega;
+    }
+
+    // main meetod
     public static void main(String[] args) {
         // defineerime muutujad
         // lubame kasutajale sisestada midagi
         Scanner sisend = new Scanner(System.in);
         // selgitame kasutajale, mida tuleb sisestada
-        System.out.println("Täringute arv: ");
+        System.out.println("Mitu korda kuvada reklaamlauset: ");
         // salvestame kasutaja sisestatud väärtus
-        int taringuteArv = sisend.nextInt();
-        // korduv tegevus
-        // kontroll
-        while (true) {
-            int taring = (int) Math.round(Math.random() * 6);
-            System.out.println(taring);
-            if (taring == taringuteArv) {
-                break;
-            }
+        int kordamine = sisend.nextInt();
+        // samuti sisendid
+        sisend = new Scanner(System.in);
+        System.out.println("Sisesta reklaamlause: ");
+        String lause = sisend.nextLine();
+        // kordamine
+        int kord = 1;
+        while (kord <= kordamine) {
+            System.out.println(banner(lause));
+            kord++;
         }
     }
 }
