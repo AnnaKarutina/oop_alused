12cd6d09dba1c6afaa1abf32f8213c0ec2a8f6c7
Anna Karutina <anna.karutina@khk.ee>
2021-01-11 15:02:34 +0200

    massivid - for abil massiivi läbimine

--- src/test.java
+++ src/test.java
@@ -13,12 +13,8 @@ public class test {
         numbrid[3] = 4;
         numbrid[4] = 5;
         System.out.println(numbrid.length);
-        System.out.println(numbrid[0]);
-        System.out.println(numbrid[1]);
-        System.out.println(numbrid[2]);
-        System.out.println(numbrid[3]);
-        System.out.println(numbrid[4]);
-        numbrid[5] = 6;
-        System.out.println(numbrid[4]);
+        for (int i = 0; i < numbrid.length; i++) {
+            System.out.println(numbrid[i]);
+        }
     }
 }
