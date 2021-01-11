b9e281f1e5fb0da8016457727dba64183d5e4db7
Anna Karutina <anna.karutina@khk.ee>
2021-01-11 14:54:41 +0200

    massivid - [] ja {} abil valmistatud

--- src/test.java
+++ src/test.java
@@ -1,28 +1,17 @@
 /*
- * meetod + return -  näide
+ * massiivid -  näide
  * autor - Anna Karutina
  * ülesanne 6.2
  * */
 
-import java.util.Scanner;
-
 public class test {
-    // mahlapakkideArv meetod
-    static int mahlapakkideArv(double ounteKogus) {
-        int pakkideArv = (int) Math.round(ounteKogus * 0.4 / 3);
-        return pakkideArv;
-    }
-
-    // main meetod
     public static void main(String[] args) {
-        // defineerime muutujad
-        // lubame kasutajale sisestada midagi
-        Scanner sisend = new Scanner(System.in);
-        // selgitame kasutajale, mida tuleb sisestada
-        System.out.println("Sisestage õunte kogus kilogrammides: ");
-        // salvestame kasutaja sisestatud väärtus
-        double ountekogus = sisend.nextDouble();
-        // arvutamine
-        System.out.println(mahlapakkideArv(ountekogus));
+        int[] numbrid = {1, 2, 3, 4, 5};
+        System.out.println(numbrid.length);
+        System.out.println(numbrid[0]);
+        System.out.println(numbrid[1]);
+        System.out.println(numbrid[2]);
+        System.out.println(numbrid[3]);
+        System.out.println(numbrid[4]);
     }
 }
